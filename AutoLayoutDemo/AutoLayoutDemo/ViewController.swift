58a5090bf77bdb1f5c88dfc454f0c38ae09e8cac
Ramprasad A <ramprasad179@gmail.com>
2018-07-13 15:07:44 +0530

    UI Design Modification
    Added new UI to the project

--- AutoLayoutDemo/AutoLayoutDemo/ViewController.swift
+++ AutoLayoutDemo/AutoLayoutDemo/ViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var blueView: UIView!
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var darkGreenView: UIView!
+    
+    var counter: Int = 0
+    
+    var colours: [UIColor] = [UIColor.green, UIColor.purple, UIColor.blue, UIColor.orange, UIColor.gray]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +25,42 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func blue_Tapped(_ sender: Any) {
+        if self.counter < self.colours.count {
+            self.blueView.backgroundColor = self.colours[self.counter]
+            self.counter += 1
+        } else {
+            self.counter = 0
+        }
+    }
+    
+    @IBAction func redTapped(_ sender: Any) {
+        if self.counter < self.colours.count {
+            self.redView.backgroundColor = self.colours[self.counter]
+            self.counter += 1
+        } else {
+            self.counter = 0
+        }
+    }
+    
+    @IBAction func greenTapped(_ sender: Any) {
+        if self.counter < self.colours.count {
+            self.greenView.backgroundColor = self.colours[self.counter]
+            self.counter += 1
+        } else {
+            self.counter = 0
+        }
+    }
+    
+    @IBAction func darkGreenTapped(_ sender: Any) {
+        if self.counter < self.colours.count {
+            self.darkGreenView.backgroundColor = self.colours[self.counter]
+            self.counter += 1
+        } else {
+            self.counter = 0
+        }
     }
 }
 
